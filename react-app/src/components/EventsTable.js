a81c5e4a1eea33d41500775602c2fcb191135512
baileyalo <90293555+baileyalo@users.noreply.github.com>
2021-10-24 01:59:42 -0400

    Update EventsTable.js
    Update of file

--- react-app/src/components/EventsTable.js
+++ react-app/src/components/EventsTable.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { DataGrid } from "@material-ui/data-grid";
+import '../App.css';
 
-function EventsTable() {
-    return (
-        <div></div>
+ function EventsTable(props) {
+    const columns =()=> {
+        return [
+            {field: "socketId", headerName: "Socket ID", width: 200 },
+            {field: "type", headerName: "Type", width: 200},
+            {field: "date", headerName: "Date", width: 200},
+            {field: "time", headerName: "Time", width: 180},
+            {field: "username", headerName: "Username", width: 300},
+            {field: "chatroom", headerName: "Chatroom", width: 300}
+        ].map(col =>({...col, headerAlign: "center"}));
+    }
+const rows =(content) =>{
+    if(!content) return [];
+
+    return content.map(msg => {
+        return {
+            socketId: msg.socketId,
+            type: msg.type,
+            date: msg.date,
+            time: msg.time,
+            username: msg.username,
+            chatroom: msg.chatroom
+        }
+    });
+}
+   return (
+
+        <div className = "dashboard-table">
+        <DataGrid rows={rows(props.content)}
+        columns={columns()} pageSize={25}   
+        rowsPerPageOptions={[25]}/>
+
+        </div>
     );
 }
 
